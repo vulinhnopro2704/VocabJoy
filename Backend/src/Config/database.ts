f8ba46bdd3c98cc0556efb9db1d8934adaa8cbf4
0914103372 <nguyenduyhair2004@gmail.com>
2024-09-19 10:52:38 +0700

    VOC-2 init sr backend

---
+++ Backend/src/Config/database.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+
+
+dotenv.config()
+const url:string = process.env.Mongo_URL ||""
+
+
+
+export const connectDb = async ():Promise<void>=>{
+    try {
+        await mongoose.connect(url)
+        console.log("connect success")
+    } catch (error) {
+        console.log(error)        
+    }
+} 
+
---
+++ Backend/src/index.ts
@@ -0,0 +1,27 @@
+
+import express, { Express, Request, Response , Application } from 'express';
+import dotenv from 'dotenv';
+import { main_route } from './Routes';
+import { connectDb } from './Config/database';
+
+
+//chay database mongoose
+connectDb()
+
+dotenv.config();
+
+const app: Application = express();
+app.set('port',process.env.PORT||8000)
+
+
+app.use("/api",main_route)
+app.get('/', (req: Request, res: Response) => {
+  res.send('Learn English');  
+});
+
+app.use("/api",main_route)
+
+const port: number = app.get('port')
+app.listen(port, () => {
+  console.log(`Server is Ruuning at http://localhost:${port}`);
+});
