96cb2773cb2507692cfcf392bffe68a1166f14ad
0914103372 <nguyenduyhair2004@gmail.com>
2024-09-27 09:29:06 +0700

    VOC-18 hotfix:update new Model

---
+++ Backend/src/Models/lessonUser-model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Types } from "mongoose";
+interface lessonUser {
+    userId: mongoose.Types.ObjectId,
+    lessonId: mongoose.Types.ObjectId,
+    number_of_world:Number
+}
+
+
+const lessonUserSchema = new Schema<lessonUser>({
+    userId: {
+        type: Schema.Types.ObjectId, 
+        required: true,
+        ref: 'user' 
+    },
+    lessonId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'lesson' 
+    },
+    number_of_world:{
+        type:Number,
+        default:0
+    }
+});
+const LessonUser = mongoose.model('lessonuser', lessonUserSchema);
+
+module.exports = LessonUser;
