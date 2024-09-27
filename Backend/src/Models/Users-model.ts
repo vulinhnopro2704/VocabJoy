3248173ed7237f2ef375a0fdd6f213c8d2763982
0914103372 <nguyenduyhair2004@gmail.com>
2024-09-27 08:51:35 +0700

    VOC-18 update model

--- Backend/src/Models/Users-model.ts
+++ Backend/src/Models/Users-model.ts
@@ -2,56 +2,80 @@ import { Schema,Model,model, Types } from "mongoose";
 import { type } from "os";
 
 
-interface user{
-    isDeleted:boolean,
-    email:string,
-    name:string,
-    date:Date,
-    phone:string,
-    account:{
-        password:string,
-        otp:string,
-        otpExp:Date
-    }
-}
-
-
-const userSchema:Schema<user> = new Schema({
-    name:{
-        type:String,
-        require:true,
-        unique:true
+interface user {
+    isDeleted: boolean
+    email: string
+    name: string
+    date: Date
+    phone: string
+    account: {
+      password: string;
+      otp: string;
+      otpExp: Date;
     },
-    email:{
-        type:String,
-        unique:true
+    lesson:{
+        lesson: Types.ObjectId,
+        number_of_world:number
+    }[]
+    ,
+    vocabulary: {
+      vocabulary: Types.ObjectId
+      count: number
+    }[]
+  }
+  
+  const userSchema: Schema<user> = new Schema({
+    name: {
+      type: String,
+      required: true,
     },
-    phone:{
-        type:String,
-        unique:true
+    email: {
+      type: String,
+      unique: true,
+      required: true, 
+    },
+    phone: {
+      type: String,
+      unique: true,
     },
     account: {
-        type: {
-            password: { type: String, required: true },
-            otp: {
-                type: String,
-                default: null,
-            },
-            otpExp: {
-                type: Date,
-                default: null,
-            },
-        },
-        required: true,
+      password: { type: String, required: true },
+      otp: {
+        type: String,
+        default: null,
+      },
+      otpExp: {
+        type: Date,
+        default: null,
+      },
     },
     isDeleted: {
-         type: Boolean, 
-         default: false 
+      type: Boolean,
+      default: false,
     },
-    date:{
-        type: Date,
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    vocabulary:{
+        type: [
+            {
+              vocab: { type: Types.ObjectId, ref: "vocab"},
+              count: { type: Number, default: 0 },
+            },
+          ],
+          default: [],
+    },
+    lesson:{
+        type:[
+            {
+                lesson:{ type:Types.ObjectId, ref:"lesson" } ,
+                number_of_world: { type:Number, default:0 }
+            }
+        ],
+        default:[]
     }
-})
+  });
 
 const User:Model<user> = model<user>('user',userSchema)
 
