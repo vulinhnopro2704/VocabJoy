7a481c703afaddce48b1c5da9fd39290bd8c968c
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-10-02 22:08:43 +0700

    VOC-20 Create header, embeded access token when call API

--- Mobile/lib/features/api/api-slice.ts
+++ Mobile/lib/features/api/api-slice.ts
@@ -1,11 +1,29 @@
 import { BACKEND_URL } from "@/constants/backend";
+import { RootState } from "@/lib/store";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: BACKEND_URL,
+	prepareHeaders: (headers, { getState }) => {
+		const token = (getState() as RootState).auth.token;
+		if (token) {
+			headers.set("Authorization", `Bearer ${token}`);
+		}
+		return headers;
+	},
+});
 
 export const apiSlice = createApi({
 	reducerPath: "api",
-	baseQuery: fetchBaseQuery({
-		baseUrl: BACKEND_URL,
-	}),
+	baseQuery: async (args, api, extraOptions) => {
+		// Exclude login and sign-up endpoints from attaching the token
+		if (args.url === "/auth/login" || args.url === "/auth/signUp") {
+			return baseQuery(args, api, extraOptions);
+		}
+		// Attach token for other endpoints
+		const result = await baseQuery(args, api, extraOptions);
+		return result;
+	},
 	endpoints: (builder) => ({
 		login: builder.mutation({
 			query: (credentials) => ({
@@ -21,6 +39,7 @@ export const apiSlice = createApi({
 				body: userData,
 			}),
 		}),
+		// Add other endpoints here
 	}),
 });
 
