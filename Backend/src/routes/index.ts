aa092fc44b489d068ddddd37228c536b92e4abd6
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-10-15 20:55:35 +0700

    VOC-21 Translate english into Vietnamese

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,9 +1,8 @@
-import express, { Express,Router } from "express"
-import auth_Route from "./auth-route"
+import express, { Router } from "express";
+import auth_Route from "./auth-route";
+import translate_Route from "./translate-route";
 
+export const main_route: Router = express.Router();
 
-
-export const main_route:Router = express.Router()
-
-
-main_route.use("/auth",auth_Route)
+main_route.use("/auth", auth_Route);
+main_route.use("/", translate_Route);
