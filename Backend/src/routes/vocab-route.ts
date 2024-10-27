f1128696a9ebcf2eb6cce20010c5d56e2f80dd6a
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-10-27 18:26:00 +0700

    VOC-16 Objective test UI and function

--- Backend/src/routes/vocab-route.ts
+++ Backend/src/routes/vocab-route.ts
@@ -1,21 +1,20 @@
-import express,{Router} from "express"
+import express, { Router } from "express";
 import {
 	addVocab,
-	createVocabController, getAllVocab,
+	createVocabController,
+	getAllVocab,
 	getVocabByNameController,
 	translateToVieController,
-	getVocabById
-} from "../controllers/vocab-controller"
+	getVocabById,
+} from "../controllers/vocab-controller";
 
+const vocab_route: Router = express.Router();
 
-const vocab_route:Router = express.Router()
-
-vocab_route.post("/create-vocabulary",createVocabController)
-vocab_route.get("/find-vocabulary",getVocabByNameController)
-vocab_route.get("/get-mean",translateToVieController)
+vocab_route.post("/create-vocabulary", createVocabController);
+vocab_route.get("/find-vocabulary", getVocabByNameController);
+vocab_route.get("/get-mean", translateToVieController);
 vocab_route.post("/", addVocab);
 vocab_route.get("/", getAllVocab);
 vocab_route.get("/:id", getVocabById);
 
-
-export default vocab_route
+export default vocab_route;
