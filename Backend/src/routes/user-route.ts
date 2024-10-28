93fbf7b1bdbdef2666e37ef86ae9797a5742f404
naeatammy <tamnguyendacnguyen@gmail.com>
2024-10-28 23:53:09 +0700

    VOC-30: if has same name sort by meaning
    VOC-30: get vocab by each level
    
    VOC-30: API get diary vocab of an user

--- Backend/src/routes/user-route.ts
+++ Backend/src/routes/user-route.ts
@@ -9,6 +9,9 @@ import {
 	saveWordForUserController,
 	getVocabToPractice,
 	updateDiary,
+	getVocabByLevel,
+	getDiary,
+	getVocabByEachLevel,
 } from "../controllers/user-controller";
 
 const user_route: Router = express.Router();
@@ -19,7 +22,10 @@ user_route.post("/save-word-for-user",saveWordForUserController);
 user_route.get("/get-user-id",getIdByTokenController);
 user_route.get("/", getAllUser);
 user_route.get("/:id/vocabs-home", getDiaryUser);
+user_route.get("/:id/get-diary", getDiary);
+user_route.get("/:id/get-vocab-each-level", getVocabByEachLevel);
 user_route.post("/:id/update-diary", updateDiary);
+user_route.get("/:id/get-vocab-by-level", getVocabByLevel);
 user_route.post("/:vocabId/:userId", addVocabToUserDiary);
 user_route.get("/:id/practice-vocab", getVocabToPractice);
 
