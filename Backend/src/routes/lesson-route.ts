aeaf9ccd4154c90b8db52211465fd026bc38b2f6
Thanh An <thanhanleton123@gmail.com>
2024-10-29 21:48:24 +0700

    CRUD for Lesson and Get 10 Vocabs From Lesson

--- Backend/src/routes/lesson-route.ts
+++ Backend/src/routes/lesson-route.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { addLesson, getAllLessons, getLessonsOfUser } from "../controllers/lesson-controller";
+import { addLesson, addVocabToLesson, deleteLessons, deleteVocabsFromLesson, getAllLessons, getLessonsOfUser, getVocabFromLessonId, updateLesson } from "../controllers/lesson-controller";
 
 const lesson_Route: Router = express.Router();
 
@@ -7,9 +7,24 @@ const lesson_Route: Router = express.Router();
 lesson_Route.get("/", getAllLessons);
 
 //ADD A LESSON
-lesson_Route.post("/addLesson", addLesson);
+lesson_Route.post("/", addLesson);
 
 //GET LESSONS OF USER
 lesson_Route.get("/:userId/lessons", getLessonsOfUser);
+
+//UPDATE LESSON
+lesson_Route.put("/:lessonId", updateLesson);
+
+//DELETE LESSONS
+lesson_Route.delete("/", deleteLessons);
+
+//ADD VOCAB TO LESSON
+lesson_Route.post("/add-vocab", addVocabToLesson);
+
+//DELETE VOCABS FROM LESSON
+lesson_Route.delete("/:lessonId/deleteVocabs", deleteVocabsFromLesson);
+
+//GET 10 VOCAB FROM LESSON
+lesson_Route.get("/:lessonId/randomVocab", getVocabFromLessonId);
 
 export default lesson_Route;
