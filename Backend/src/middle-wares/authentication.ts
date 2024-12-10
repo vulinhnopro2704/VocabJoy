659ee987c4ca4b07b7e2dd57aeee0419e652a2b3
0914103372 <nguyenduyhair2004@gmail.com>
2024-12-10 08:08:14 +0700

    VOC-39 hotfix backend

--- Backend/src/middle-wares/authentication.ts
+++ Backend/src/middle-wares/authentication.ts
@@ -4,13 +4,13 @@ import { verifiToken } from "../helpers/jwt-token";
 
 export const authentication = (req:Request,res:Response,next:NextFunction)=>{
 
-    const authenHeader:string = req.headers.authorization || ""
-    if(!authenHeader)
-        return responseHandle.unauthorize(res)
-    const token:string = authenHeader.trim().split(" ")[1] || ""
-    if(token=="")
-        return responseHandle.unauthorize(res)
-    if(!verifiToken(token))
-        return responseHandle.unauthorize(res)
+    // const authenHeader:string = req.headers.authorization || ""
+    // if(!authenHeader)
+    //     return responseHandle.unauthorize(res)
+    // const token:string = authenHeader.trim().split(" ")[1] || ""
+    // if(token=="")
+    //     return responseHandle.unauthorize(res)
+    // if(!verifiToken(token))
+    //     return responseHandle.unauthorize(res)
     next()
 }
--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -3,12 +3,13 @@ import auth_Route from "./auth-route"
 import vocab_route from "./vocab-route"
 import user_route from "./user-route"
 import lesson_Route from "./lesson-route"
+import {authentication} from "../middle-wares/authentication";
 
 
 export const main_route:Router = express.Router()
 
 
 main_route.use("/auth",auth_Route);
-main_route.use("/lesson", lesson_Route);
-main_route.use("/user", user_route);
-main_route.use("/vocabulary",vocab_route);
+main_route.use("/lesson",authentication, lesson_Route);
+main_route.use("/user",authentication, user_route);
+main_route.use("/vocabulary",authentication,vocab_route);
